9ab3e6b6df5c368479123e19d344531e11c83066
Anujmishra2005 <anujmishraan2005@gmail.com>
2024-09-01 23:35:02 +0530

    New MERN-CHAT-APP

---
+++ frontend/src/hooks/useListenMessages.js
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import { useSocketContext } from "../context/SocketContext";
+import useConversation from "../zustand/useConversation";
+import notificationSound from "../assets/sounds/notification.mp3";
+
+const useListenMessages = () => {
+    const { socket } = useSocketContext();
+    const { setMessages } = useConversation();
+
+    useEffect(() => {
+        const handleNewMessage = (newMessage) => {
+            newMessage.shouldShake = true;
+            const sound = new Audio(notificationSound);
+            sound.play();
+            setMessages(prevMessages => [...prevMessages, newMessage]);
+        };
+
+        socket?.on("newMessage", handleNewMessage);
+
+        return () => {
+            socket?.off("newMessage", handleNewMessage);
+        };
+    }, [socket, setMessages]);
+};
+
+export default useListenMessages;
